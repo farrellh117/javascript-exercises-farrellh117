c392e0a52c5aa737aba331e2c6181750de838ae1
farrellh117 <farrelhafidi035@gmail.com>
2025-02-25 16:51:30 +0700

    Berhasil mengerjakan test repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -14,6 +14,9 @@ repeatString("hey", 3);
 repeatString("hello", 10);
 repeatString("hi", 1);
 repeatString("bye", 0);
+repeatString("goodbye", -1);
+repeatString("hey", 0);
+repeatString("", 10);
 
 // Do not edit below this line
 module.exports = repeatString;
