112ce30632077357187cdd0d3a10e9bf11db1d3f
farrellh117 <farrelhafidi035@gmail.com>
2025-02-25 16:26:16 +0700

    Berhasil mengerjakan repeatString test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeat) {
+    let result = "";
+    for(i = 0; i < repeat; i++) {
+        result += text;
+    }
+    return result;
 };
+
+repeatString("Hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
