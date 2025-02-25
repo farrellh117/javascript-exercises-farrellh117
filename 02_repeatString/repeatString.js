8f1ac537e4687daf54bf368bab0f0f04f33f4374
farrellh117 <farrelhafidi035@gmail.com>
2025-02-25 16:36:11 +0700

    Tambah logika jika pengulangan dibawah 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,8 @@
 const repeatString = function(text, repeat) {
+    if(repeat < 0) {
+        return "ERROR";
+    }
+
     let result = "";
     for(i = 0; i < repeat; i++) {
         result += text;
@@ -6,7 +10,10 @@ const repeatString = function(text, repeat) {
     return result;
 };
 
-repeatString("Hey", 3);
+repeatString("hey", 3);
+repeatString("hello", 10);
+repeatString("hi", 1);
+repeatString("bye", 0);
 
 // Do not edit below this line
 module.exports = repeatString;
