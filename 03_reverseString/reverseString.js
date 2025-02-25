c16745012c146d3272cdb6af77b701c180b44e3a
farrellh117 <farrelhafidi035@gmail.com>
2025-02-25 22:11:26 +0700

    Berhasil mengerjakan test 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let text = "";
 
+    for(let i = str.length - 1; i >= 0; i--) {
+        text += str[i];
+    }
+    return text;
 };
+
+reverseString("hello");
+reverseString("hello there");
+reverseString("123! abc! Hello, Odinite.");
+reverseString("");
 
 // Do not edit below this line
 module.exports = reverseString;
