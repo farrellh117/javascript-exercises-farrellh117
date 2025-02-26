d0c2b1dde9d3c15f853297f8b5bb0ff878572dba
farrellh117 <farrelhafidi035@gmail.com>
2025-02-26 20:38:03 +0700

    Berhasil mengerjakan test 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+/*
+Amcil input array dari function removeFromArray
+Pilih indeks yang ingin dihapus
+Hapus indeks array yang dipilih
+*/
+
+const removeFromArray = function(arr, ...valuesDel) {
+    return arr.filter(item => !valuesDel.includes(item));
 };
+
+removeFromArray([1,2,3,4], 3);
+removeFromArray([1,2,3,4], 3, 2);
+removeFromArray([1,2,2,4], 2);
+removeFromArray([1,2,3,4], 7, "tacos");
+removeFromArray([1,2,3,4], 7, 2);
+removeFromArray([1,2,3,4], 1, 2, 3, 4);
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3);
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
