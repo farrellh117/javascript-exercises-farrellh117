a7bdc61974afe116990df1b1101c9c9c9e90f15e
farrellh117 <farrelhafidi035@gmail.com>
2025-03-03 08:55:29 +0700

    Berhasil mengerjakan test 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if((year % 4 == 0) && (year % 100 !== 0) || (year % 400 == 0)) {
+        year = true;
+    } else {
+        year = false;
+    }
 
+    return year;
 };
+
+leapYears(1996);
+leapYears(1997);
+leapYears(34992);
+leapYears(1900);
+leapYears(1600);
+leapYears(700);
 
 // Do not edit below this line
 module.exports = leapYears;
