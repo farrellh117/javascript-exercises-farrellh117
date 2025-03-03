229048ee3890597d32e950e625f2af731586ba99
farrellh117 <farrelhafidi035@gmail.com>
2025-03-03 09:54:28 +0700

    Berhasil mengerjakan test 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  temperature = (temperature - 32) * 5/9;
+  let tempRound = Math.round(temperature * 10) / 10;
+
+  return tempRound;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  temperature = (9/5) * temperature + 32;
+  let tempRound = Math.round(temperature * 10) / 10;
+
+  return tempRound;
 };
+
+convertToCelsius(32);
+convertToCelsius(100);
+convertToCelsius(-100);
+convertToFahrenheit(0);
+convertToFahrenheit(73.2);
+convertToFahrenheit(-10)
 
 // Do not edit below this line
 module.exports = {
